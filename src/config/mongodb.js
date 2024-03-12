8a5b30fd3d0733fab389fdf11f71798f1bcd42bd
TNV <toanrui64@gmail.com>
2024-03-12 23:07:00 +0700

    Init Code Connect_MongoDB_ENV_Variables

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,44 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+// eslint-disable-next-line no-unused-vars
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from '~/config/environment'
+
+// Khởi tạo 1 object trelloDatabaseInstance ban đầu với giá trị null (vì ta chưa connect)
+let trelloDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  // Lưu ý: đọc thêm tài liệu dưới đường link đây
+  // https://www.mongodb.com/docs/drivers/node/current/fundamentals/stable-api/
+  // serverApi: {
+  //   version: ServerApiVersion.v1,
+  //   strict : true,
+  //   deprecationErrors: true
+  // }
+})
+
+// Kết nối tới Database
+export const CONNECT_DB = async () => {
+  // gọi kết nối tới MongoDB Atlas với URI đã khai báo trong thân của mongoClientInstance
+  await mongoClientInstance.connect()
+
+  // Kết nối thành công thì lấy ra Database theo tên và gán ngược nó lại vào biến
+  // trelloDatabaseInstance ở trên
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+// Đóng kết nối Database khi cần
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
+
+// Function GET_DB (không async) này có nhiệm vụ export ra Trello Database Instance
+// sau khi đã connect thành công tới MongoDB để ta sử dụng ở nhiều nơi khác nhau
+// trong code
+// Lưu ý: phải đảm bảo chỉ luôn gọi GET_DB này sau khi đã connect thành công tới MongoDB
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to the database first !')
+  return trelloDatabaseInstance
+}
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
@@ -5,29 +6,51 @@
  */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    // eslint-disable-next-line no-console
+    console.log(`3. Hello ${env.AUTHOR}, Back-End Server is running successfully at http://${ env.APP_HOST }:${ env.APP_PORT }/`)
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello TNV, I am running at http://${ hostname }:${ port }/`)
-})
+  // Thực hiện các tác vụ Cleanup trước khi dùng Server
+  exitHook(() => {
+    console.log('4. Server is shutting down ...')
+    CLOSE_DB()
+    console.log('5. Disconnected from MongoDB Atlas Cloud !')
+  })
+}
+
+// chỉ khi kết nối tới Database thành công thì Start Server Back-End lên.
+// Immediately-Invoked / Anonymous Async Functions (IIFE)
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB Cloud Atlas ...')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB Cloud Atlas !')
+
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+
+// // chỉ khi kết nối tới Database thành công thì Start Server Back-End lên.
+// console.log('1. Connecting to MongoDB Cloud Atlas ...')
+// CONNECT_DB()
+//   .then(() => console.log('2. Connected to MongoDB Cloud Atlas !'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
