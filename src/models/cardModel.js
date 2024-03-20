1e37c98b9b9ca457566a5b8acbff4c4ffcdfbb06
TNV <toanrui64@gmail.com>
2024-03-20 18:07:19 +0700

    Create Model & Create API New Board

---
+++ src/models/cardModel.js
@@ -0,0 +1,26 @@
+/**
+ * Updated by trungquandev.com's author on Oct 8 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+
+import Joi from 'joi'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
+
+const CARD_COLLECTION_NAME = 'boards'
+const CARD_COLLECTION_SCHEMA = Joi.object({
+  boardId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+  columnId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+
+  title: Joi.string().required().min(3).max(50).trim().strict(),
+  description: Joi.string().optional(),
+
+  createAt: Joi.date().timestamp('javascript').default(Date.now),
+  updateAt: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean().default(false)
+})
+
+export const cardModel = {
+  CARD_COLLECTION_NAME,
+  CARD_COLLECTION_SCHEMA
+}
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -5,6 +5,7 @@
  */
 
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
 
 const createNew = async (reqBody) => {
   // eslint-disable-next-line no-useless-catch
@@ -16,12 +17,18 @@ const createNew = async (reqBody) => {
     }
 
     // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
-    // ...
+    const createdBoard = await boardModel.createNew(newBoard)
+    // Trả về kết quả của insertOne()
+    console.log(createdBoard)
+
+    // Lấy bản ghi board sau khi gọi (tùy mục đích dự án mà bạn có cần hay không nha !)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    console.log(getNewBoard)
 
     // Làm thêm các xử lý logic khác với các Collection khác tùy đặc thù dự án ...
     // Bắn email, notification về cho admin khi có 1 board mới được tạo ra ...
     // Trả kết quả về trong Service (lưu ý là luôn phải có return, nếu không là nó cứ request đến chết web luôn đó)
-    return newBoard
+    return getNewBoard
   } catch (error) { throw error }
 }
 
